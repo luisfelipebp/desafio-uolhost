80e9585a47cb4f4a5a084675df1d30c4ada6c187
luisfelipebp <luisfelipebarbosa2013@gmail.com>
2023-12-21 15:00:03 -0300

    Removendo configuração inicial de popular o banco de dados automaticamente

--- src/main/java/com/luisfelipebp/DesafioUolhostApplication.java
+++ src/main/java/com/luisfelipebp/DesafioUolhostApplication.java
@@ -18,10 +18,10 @@ public class DesafioUolhostApplication {
 	@Bean
 	CommandLineRunner initDatabase(JogadorRepository jogadorRepository) {
 		return args -> {
-			Jogador jogador1 = new Jogador(null, "Luis Felipe", "luis@gmail.com","2299879786",GrupoEnum.LIGA_DA_JUSTICA, "Hulk");
-			Jogador jogador2 = new Jogador(null, "Jonas", "jonas@gmail.com","2299879783", GrupoEnum.VINGADORES, "Capitão América");
-			jogadorRepository.save(jogador1);
-			jogadorRepository.save(jogador2);
+//			Jogador jogador1 = new Jogador(null, "Luis Felipe", "luis@gmail.com","2299879786",GrupoEnum.LIGA_DA_JUSTICA, "Hulk");
+//			Jogador jogador2 = new Jogador(null, "Jonas", "jonas@gmail.com","2299879783", GrupoEnum.VINGADORES, "Capitão América");
+//			jogadorRepository.save(jogador1);
+//			jogadorRepository.save(jogador2);
 		};
 	}
 
