ee9eeda398b7bf797b71403d943acbb8079c5bc8
luisfelipebp <luisfelipebarbosa2013@gmail.com>
2023-12-21 14:57:29 -0300

    Criação da classe jogador e do enum do grupo

---
+++ src/main/java/com/luisfelipebp/model/Jogador.java
@@ -0,0 +1,40 @@
+package com.luisfelipebp.model;
+
+import com.luisfelipebp.model.DTO.JogadorDTO;
+import com.luisfelipebp.model.enums.GrupoEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "players")
+public class Jogador {
+
+    public Jogador(JogadorDTO jogadorDTO){
+        this.nome = jogadorDTO.nome();
+        this.email = jogadorDTO.email();
+        this.telefone = jogadorDTO.telefone();
+        this.grupo = jogadorDTO.grupo();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String telefone;
+
+    @Enumerated(EnumType.STRING)
+    private GrupoEnum grupo;
+
+    private String codinome;
+}
---
+++ src/main/java/com/luisfelipebp/model/enums/GrupoEnum.java
@@ -0,0 +1,7 @@
+package com.luisfelipebp.model.enums;
+
+public enum GrupoEnum {
+
+    VINGADORES,
+    LIGA_DA_JUSTICA
+}
